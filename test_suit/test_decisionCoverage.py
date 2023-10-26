eddd3d82636e9653e81d7d5130aa9fd31462b37a
Long Huynh <longbhuynh019@gmail.com>
2023-10-26 18:32:40 -0400

    statement and decision coverage

---
+++ test_suit/test_decisionCoverage.py
@@ -0,0 +1,50 @@
+import unittest
+from isTriangle import Triangle
+# import sys
+# sys.path.append("..")
+# from src.Triangle import Triangle
+
+class StatementCoverageTest(unittest.TestCase):
+    def test1(self):
+        actual = Triangle.classify(-2, 10, 10)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+    
+    def test2(self):
+        actual = Triangle.classify(6, 6, 7)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+
+    def test3(self):
+        actual = Triangle.classify(6, 9, 6)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+
+    def test4(self):
+        actual = Triangle.classify(4, 9, 9)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+
+    def test5(self):
+        actual = Triangle.classify(3, 4, 6)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
+
+    def test6(self):
+        actual = Triangle.classify(2, -2, -2)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+    def test7(self):
+        actual = Triangle.classify(10, 10, 10)
+        expected = Triangle.Type.EQUILATERAL
+        self.assertEqual(actual, expected)
+
+    def test8(self):
+        actual = Triangle.classify(2, 3, 6)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_suit/test_statementCoverage.py
@@ -0,0 +1,40 @@
+import unittest
+from isTriangle import Triangle
+# import sys
+# sys.path.append("..")
+# from src.Triangle import Triangle
+
+class StatementCoverageTest(unittest.TestCase):
+    def test1(self):
+        actual = Triangle.classify(10, 10, 10)
+        expected = Triangle.Type.EQUILATERAL
+        self.assertEqual(actual, expected)
+    
+    def test2(self):
+        actual = Triangle.classify(6, 4, 7)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
+
+    def test3(self):
+        actual = Triangle.classify(4, 9, 9)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+
+    def test4(self):
+        actual = Triangle.classify(2, -2, -2)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+    def test5(self):
+        actual = Triangle.classify(7, 7, 2)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+
+    def test6(self):
+        actual = Triangle.classify(7, 6, 7)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
